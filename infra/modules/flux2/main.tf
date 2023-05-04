4666bfac555d5622fe8e5f3370f62f044a9d57b9
Parthiv Seetharaman <pachum99@myrdd.info>
2023-05-04 09:08:54 -0700

    remove flux code for now

--- infra/modules/flux2/main.tf
+++
@@ -1,5 +0,0 @@
-resource "flux_bootstrap_git" "this" {
-  path                   = "clusters/my-cluster"
-  network_policy         = true
-  # kustomization_override = file("${path.module}/kustomization.yaml")
-}
