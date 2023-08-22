017272d60638658155c613409155f5c90a84ff08
Shea Levy <shea@shealevy.com>
2023-08-22 08:01:10 -0400

    Add Plutus benchmarking machine config

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     capsules.url = "github:input-output-hk/devshell-capsules";
     devenv.url = "github:cachix/devenv";
+    disko.url = "github:nix-community/disko";
   };
   outputs = inputs@{ self, flake-parts, devenv, capsules, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -14,6 +15,52 @@
       # Raw flake outputs (generally not system-dependent)
       flake = {
         devenvModules = import ./devenvModules;
+        nixosConfigurations.plutus-bench = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./nixos/ax41-nvme.nix
+            {
+              system.stateVersion = "23.05";
+              hetzner-dedicated.nic = {
+                ipv4 = "65.108.236.230";
+                netmask4 = 26;
+                gateway4 = "65.108.236.193";
+                ipv6 = "2a01:4f9:1a:b0de::2";
+                netmask6 = 64;
+                mac = "a8:a1:59:a2:95:40";
+              };
+              users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/fJqgjwPG7b5SRPtCovFmtjmAksUSNg3xHWyqBM4Cs shlevy@shlevy-laptop" ];
+              networking.nameservers = [ "185.12.64.1" "185.12.64.2" "2a01:4ff:ff00::add:1" "2a01:4ff:ff00::add:2" ];
+            }
+            ({ pkgs, lib, ... }: {
+              environment.systemPackages = [ pkgs.git ];
+              nixpkgs.config.permittedInsecurePackages = [
+                "nodejs-16.20.2"
+              ];
+              nix.settings = {
+                max-jobs = 12;
+                cores = 0;
+                sandbox = true;
+                substituters = lib.mkAfter [ "https://cache.iog.io" ];
+                trusted-public-keys = [
+                  "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+                ];
+                trusted-users = [ "root" ];
+                builders-use-substitutes = true;
+                experimental-features = [ "nix-command" "flakes" ];
+                bash-prompt = ''\n\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w \[\033[01;31m\](dev-shell)\[\033[01;32m\]]\$ \[\033[0m\]'';
+              };
+              services.github-runner = {
+                enable = true;
+                extraLabels = [ "plutus-benchmark" ];
+                name = "plutus-benchmark";
+                tokenFile = "/root/runner-pat";
+                url = "https://github.com/input-output-hk/plutus";
+              };
+            })
+          ];
+        };
       };
       # Flake outputs that will be split by system
       perSystem = { config, pkgs, inputs', self', ... }: {
---
+++ nixos/ax41-nvme.nix
@@ -0,0 +1,132 @@
+{ inputs, config, lib, ... }: let
+  inherit (lib) mkOption types;
+
+  cfg = config.hetzner-dedicated.nic;
+in assert (inputs ? disko); {
+  imports = [ inputs.disko.nixosModules.disko ];
+
+  options.hetzner-dedicated.nic = {
+    ipv4 = mkOption {
+      description = "The IPv4 address of the main NIC";
+      type = types.str;
+      example = "65.108.236.230";
+    };
+    netmask4 = mkOption {
+      description = "The IPv4 netmask of the main NIC";
+      type = types.int;
+      example = 26;
+    };
+    gateway4 = mkOption {
+      description = "The IPv4 network gateway of the main NIC";
+      type = types.str;
+      example = "65.108.236.193";
+    };
+    ipv6 = mkOption {
+      description = "The IPv6 address of the main NIC";
+      type = types.str;
+      example = "2a01:4f9:1a:b0de::2";
+    };
+    netmask6 = mkOption {
+      description = "The IPv6 netmask of the main NIC";
+      type = types.int;
+      example = 64;
+    };
+    gateway6 = mkOption {
+      description = "The IPv6 network gateway of the main NIC";
+      type = types.str;
+      default = "fe80::1";
+    };
+    mac = mkOption {
+      description = "The MAC address of the main NIC";
+      type = types.str;
+      example = "a8:a1:59:a2:95:40";
+    };
+  };
+
+  config = {
+    boot = {
+      initrd.availableKernelModules = [ "ahci" "nvme" "ext4" ];
+
+      kernelModules = [ "kvm-amd" ];
+
+      loader.grub = {
+        copyKernels = true;
+        devices = [ "/dev/disk/by-path/pci-0000:2c:00.0-nvme-1" ];
+        enable = true;
+        fsIdentifier = "uuid";
+      };
+    };
+
+    disko.devices.disk = {
+      disk0 = {
+        device = "/dev/disk/by-path/pci-0000:2c:00.0-nvme-1";
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "boot";
+              start = "0";
+              end = "1M";
+              part-type = "primary";
+              flags = [ "bios_grub" ];
+            }
+            {
+              name = "root";
+              start = "1M";
+              end = "100%";
+              fs-type = "ext4";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            }
+          ];
+        };
+      };
+      disk1 = {
+        device = "/dev/disk/by-path/pci-0000:2d:00.0-nvme-1";
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "nix";
+              start = "0";
+              end = "100%";
+              fs-type = "ext4";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/nix";
+              };
+            }
+          ];
+        };
+      };
+    };
+
+    systemd.network.links."10-main" = {
+      matchConfig.PermanentMACAddress = cfg.mac;
+      linkConfig.Name = "main";
+    };
+
+    networking = {
+      interfaces.main = {
+        ipv4.addresses = [ { address = cfg.ipv4; prefixLength = cfg.netmask4; } ];
+
+        ipv6.addresses = [ { address = cfg.ipv6; prefixLength = cfg.netmask6; } ];
+      };
+
+      defaultGateway = cfg.gateway4;
+
+      defaultGateway6 = {
+        address = cfg.gateway6;
+        interface = "main";
+      };
+    };
+
+    services.openssh.enable = true;
+  };
+}
