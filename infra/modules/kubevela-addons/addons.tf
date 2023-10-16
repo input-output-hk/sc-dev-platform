541eee3f895a42ca2f5ef75e84275b818834d3ce
Daniel KT <daniel.thagard@tweag.io>
2023-10-16 15:31:56 -0400

    remove leftover postgres operator config

--- infra/modules/kubevela-addons/addons.tf
+++ infra/modules/kubevela-addons/addons.tf
@@ -1,12 +1,3 @@
-data "kubectl_file_documents" "postgres-operator" {
-  content = file("${path.module}/postgres-operator.yaml")
-}
-
-resource "kubectl_manifest" "postgres-operator" {
-  for_each  = data.kubectl_file_documents.postgres-operator.manifests
-  yaml_body = each.value
-}
-
 data "kubectl_file_documents" "otel-operator" {
   content = file("${path.module}/otel-operator.yaml")
 }
