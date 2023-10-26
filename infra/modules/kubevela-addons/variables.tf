d12b6a69b4b0a95392eed1052c7fde653fbfbb71
Renê Barbosa <renebarbosafl@gmail.com>
2023-10-26 09:54:15 -0300

    kubevela-addons module refactor

--- infra/modules/kubevela-addons/variables.tf
+++ infra/modules/kubevela-addons/variables.tf
@@ -1,7 +1,30 @@
+variable "aws_profile" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_version" {
+  type = string
+}
+
+variable "cluster_endpoint" {
+  type = string
+}
+
+variable "cluster_certificate_authority_data" {
+  type = string
+}
+
+variable "oidc_provider_arn" {
+  type = string
+}
+
 variable "namespace" {
-  description = "Set the namespace that kubevela is deployed to"
-  type        = string
-  default     = "vela-system"
+  type    = string
+  default = "vela-system"
 }
 
 variable "helm_defaults" {
@@ -10,8 +33,19 @@ variable "helm_defaults" {
   default     = {}
 }
 
-variable "cluster-name" {
-  description = "Name of the Kubernetes cluster"
-  default     = "sample-cluster"
+variable "velaux_domain" {
+  description = "domain where VelaUX will be hosted"
   type        = string
+}
+
+variable "dex_client_id" {
+  description = "client ID for dex provider"
+  type        = string
+  sensitive   = true
+}
+
+variable "dex_client_secret" {
+  description = "client secret for dex provider"
+  type        = string
+  sensitive   = true
 }
