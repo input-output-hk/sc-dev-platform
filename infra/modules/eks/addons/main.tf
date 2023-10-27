25ec51bd66eed7ccb9e8435553b028d7daa4f52c
Renê Barbosa <renebarbosafl@gmail.com>
2023-10-26 22:24:40 -0300

    disabling otel-operator addon because it's not used anymore

--- infra/modules/eks/addons/main.tf
+++ infra/modules/eks/addons/main.tf
@@ -171,34 +171,34 @@ module "eks_addon_traefik_load_balancer" {
   ]
 }
 
-module "eks_addon_otel_operator" {
-  count   = try(var.eks_addons.enable_otel_operator, false) ? 1 : 0
-  source  = "aws-ia/eks-blueprints-addon/aws"
-  version = "1.1.1"
+#module "eks_addon_otel_operator" {
+#  count   = try(var.eks_addons.enable_otel_operator, false) ? 1 : 0
+#  source  = "aws-ia/eks-blueprints-addon/aws"
+#  version = "1.1.1"
+#
+# chart            = local.eks_addons.otel_operator.chart
+#  chart_version    = try(var.eks_addons.otel_operator.chart_version, local.eks_addons.otel_operator.chart_version)
+#  repository       = try(var.eks_addons.otel_operator.repository, local.eks_addons.otel_operator.repository)
+#  description      = try(var.eks_addons.otel_operator.description, local.eks_addons.otel_operator.description)
+#  namespace        = try(var.eks_addons.otel_operator.namespace, local.eks_addons.otel_operator.namespace)
+#  create_namespace = try(var.eks_addons.otel_operator.create_namespace, local.helm_create_namespace)
+#  values           = try(var.eks_addons.otel_operator.values, local.eks_addons.otel_operator.values)
+#  set              = try(var.eks_addons.otel_operator.set, local.eks_addons.otel_operator.set)
+#  wait             = try(var.eks_addons.otel_operator.wait, local.helm_wait)
 
-  chart            = local.eks_addons.otel_operator.chart
-  chart_version    = try(var.eks_addons.otel_operator.chart_version, local.eks_addons.otel_operator.chart_version)
-  repository       = try(var.eks_addons.otel_operator.repository, local.eks_addons.otel_operator.repository)
-  description      = try(var.eks_addons.otel_operator.description, local.eks_addons.otel_operator.description)
-  namespace        = try(var.eks_addons.otel_operator.namespace, local.eks_addons.otel_operator.namespace)
-  create_namespace = try(var.eks_addons.otel_operator.create_namespace, local.helm_create_namespace)
-  values           = try(var.eks_addons.otel_operator.values, local.eks_addons.otel_operator.values)
-  set              = try(var.eks_addons.otel_operator.set, local.eks_addons.otel_operator.set)
-  wait             = try(var.eks_addons.otel_operator.wait, local.helm_wait)
+#  depends_on = [
+#    module.eks_addons
+#  ]
+#}
 
-  depends_on = [
-    module.eks_addons
-  ]
-}
+#data "kubectl_file_documents" "otel_operator" {
+#  content = file("${path.module}/manifests/otel_traits.yaml")
+#}
 
-data "kubectl_file_documents" "otel_operator" {
-  content = file("${path.module}/manifests/otel_traits.yaml")
-}
-
-resource "kubectl_manifest" "otel_operator" {
-  for_each  = try(var.eks_addons.enable_otel_operator, false) ? data.kubectl_file_documents.otel_operator.manifests : {}
-  yaml_body = each.value
-}
+#resource "kubectl_manifest" "otel_operator" {
+#  for_each  = try(var.eks_addons.enable_otel_operator, false) ? data.kubectl_file_documents.otel_operator.manifests : {}
+#  yaml_body = each.value
+#}
 
 module "eks_addon_kubevela_controller" {
   count   = try(var.eks_addons.enable_kubevela_controller, false) ? 1 : 0
