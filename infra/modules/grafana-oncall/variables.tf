46b820243ed570d2ed2c7712bf3906a1e9f6db98
Olaniyi Oshunbote <oshunboteo@gmail.com>
2023-12-13 21:34:59 +0000

    Add Grafana Oncall module and terragrunt config.

---
+++ infra/modules/grafana-oncall/variables.tf
@@ -0,0 +1,27 @@
+variable "oncall_access_token" {
+  description = "Access token for Grafana Oncall system"
+  type        = string
+  sensitive   = true
+}
+
+variable "usernames" {
+  description = "List of users in the Oncall schedule"
+  type        = list(string)
+  default     = [ ]
+}
+
+variable "team_name" {
+  description = "Name of team using the oncall schedule"
+  type        = string
+}
+
+variable "team_email" {
+  description = "Email address of team using the oncall schedule"
+  type        = string
+}
+
+variable "slack_channels" {
+  description = "List of Slack channels notifications/alerts will be sent to"
+  type        = list(string)
+  default     = [ ]
+}
