d31e774decc164e43934c115a199e0c6b524373a
Olaniyi Oshunbote <oshunboteo@gmail.com>
2023-12-14 19:45:41 +0000

    Update config for on call to include all scde members, notification regex, etc

--- infra/modules/grafana-oncall/shift.tf
+++ infra/modules/grafana-oncall/shift.tf
@@ -3,14 +3,16 @@ resource "grafana_oncall_on_call_shift" "week_shift" {
 
   name       = "${var.team_name} Week Shift"
   type       = "rolling_users"
-  start      = "2023-12-18T00:00:00"
+  start      = var.oncall_schedule_start_date
+  time_zone  = "UTC"
   duration   = 60 * 60 * 24 // 24 hours
   frequency  = "weekly"
-  interval   = 2
+  interval   = 1
   by_day     = ["MO", "TU", "WE", "TH", "FR", "SA", "SU"]
   week_start = "MO"
+
   rolling_users = [for username, user in data.grafana_oncall_user.users : [user.id]]
-  time_zone = "UTC"
+  start_rotation_from_user_index = 1
 }
 
 resource "grafana_oncall_schedule" "team_schedule" {
