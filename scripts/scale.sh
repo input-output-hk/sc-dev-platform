1851b04ac2dba8f9a875c14047d55eb743aada9a
Fenton Haslam <fenton.haslam@iohk.io>
2023-12-20 07:32:46 -0500

    added new changes to update dev cluster

--- scripts/scale.sh
+++ scripts/scale.sh
@@ -1,6 +1,24 @@
+#!/bin/bash
+
+# Function to display an error message and exit
+error_exit() {
+  echo "Error: $1" >&2
+  exit 1
+}
+
+# Check if the required command-line arguments are provided
+if [ $# -ne 3 ]; then
+  error_exit "Usage: $0 <environment> <cluster> <namespace>"
+fi
 
 env=$1
 cluster=$2
+namespace=$3
+
+# Check if the 'terraform' command is available
+if ! command -v terraform &>/dev/null; then
+  error_exit "'terraform' command not found. Please install Terraform."
+fi
 
 cluster_location=$(pwd)/infra/us-east-1/$env/eks/$cluster/eks
 eks_cluster_name=$(basename "$(cd "$cluster_location" && terragrunt output cluster_name)" | sed 's/"//g')
@@ -8,4 +26,15 @@ eks_cluster_name=$(basename "$(cd "$cluster_location" && terragrunt output clust
 
 aws eks --region us-east-1 update-kubeconfig --name "$eks_cluster_name"
 
-kubectl --context arn:aws:eks:us-east-1:677160962006:cluster/scde-dev-us-east-1 -n marlowe-staging get deploy -oname | grep indexer | while read -r deploy; do kubectl --context scde-prod-blue -n marlowe-staging scale $deploy --replicas=0; done
+read -p "Do you want to scale the indexer up? (y/n):" scale_context
+
+if [[ $scale_context == "y" || $scale_context == "Y"]]; then
+    kubectl --context "$eks_cluster_name" -n marlowe-staging get deploy -oname | grep indexer | while read -r deploy; do 
+    kubectl --context scde-prod-blue -n marlowe-staging scale $deploy --replicas=1; done
+    echo "Indexer is scaled to 1 in $namespace"
+else
+    kubectl --context "$eks_cluster_name" -n marlowe-staging get deploy -oname | grep indexer | while read -r deploy; do 
+    kubectl --context scde-prod-blue -n marlowe-staging scale $deploy --replicas=0; done
+    echo "Indexer is scaled to 0 in $namespace"
+fi
+
