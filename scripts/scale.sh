b670c76816762a8ddfcb58ab94695bc00b5c64dd
Fenton Haslam <fenton.haslam@iohk.io>
2023-12-20 11:15:37 -0500

    added namespace variable

--- scripts/scale.sh
+++ scripts/scale.sh
@@ -29,12 +29,12 @@ aws eks --region us-east-1 update-kubeconfig --name "$eks_cluster_name"
 read -p "Do you want to scale the indexer up? (y/n):" scale_context
 
 if [[ $scale_context == "y" || $scale_context == "Y"]]; then
-    kubectl --context "$eks_cluster_name" -n marlowe-staging get deploy -oname | grep indexer | while read -r deploy; do 
-    kubectl --context scde-prod-blue -n marlowe-staging scale $deploy --replicas=1; done
+    kubectl --context "$eks_cluster_name" -n $namespace get deploy -oname | grep indexer | while read -r deploy; do 
+    kubectl --context scde-prod-blue -n $namespace scale $deploy --replicas=1; done
     echo "Indexer is scaled to 1 in $namespace"
 else
-    kubectl --context "$eks_cluster_name" -n marlowe-staging get deploy -oname | grep indexer | while read -r deploy; do 
-    kubectl --context scde-prod-blue -n marlowe-staging scale $deploy --replicas=0; done
+    kubectl --context "$eks_cluster_name" -n $namespace get deploy -oname | grep indexer | while read -r deploy; do 
+    kubectl --context scde-prod-blue -n $namespace scale $deploy --replicas=0; done
     echo "Indexer is scaled to 0 in $namespace"
 fi
 
