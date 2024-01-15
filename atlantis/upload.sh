0510a3364ca881b2eabb8e49a7e1dff52c4c221c
Fenton Haslam <fenton.haslam@iohk.io>
2024-01-15 15:16:38 +0000

    added new notes to shell

--- atlantis/upload.sh
+++ atlantis/upload.sh
@@ -1,7 +1,21 @@
+#!/bin/bash
+
+# Step 1: Build Docker Image
 docker build -t scde . --platform=linux/amd64
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 677160962006.dkr.ecr.us-east-1.amazonaws.com
+# Step 2: AWS ECR Login
+account_id=$(aws sts get-caller-identity --output text --query 'Account')
+region=$(aws configure get region)
+ecr_registry="${account_id}.dkr.ecr.${region}.amazonaws.com"
 
-docker tag scde:latest 677160962006.dkr.ecr.your-region.amazonaws.com/scde:latest
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${region}.amazonaws.com
 
-docker push 677160962006.dkr.ecr.us-east-1.amazonaws.com/scde:latest
+# Step 3: Tag Docker Image
+docker tag scde:latest ${ecr_registry}/scde:latest
+
+# Step 4: Push Docker Image
+docker push ${ecr_registry}/scde:latest
+
+# Additional Tips:
+echo "Make sure Docker has experimental features enabled for multi-platform builds."
+echo "Ensure AWS CLI is configured with the necessary permissions for Amazon ECR actions."
