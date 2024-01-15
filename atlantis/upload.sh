5f4a0b885e684619cb63a903a5b08adde50d3b85
Fenton Haslam <fenton.haslam@iohk.io>
2024-01-15 14:44:01 +0000

    added cahnge to terragrunt for atlantis review

--- atlantis/upload.sh
+++ atlantis/upload.sh
@@ -1,2 +1,7 @@
-aws ecr get-login-password --region us-east-1 | docker login --username fentonhaslam --password-stdin 677160962006.dkr.ecr.us-east-1.amazonaws.com
+docker build -t scde . --platform=linux/amd64
 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 677160962006.dkr.ecr.us-east-1.amazonaws.com
+
+docker tag scde:latest 677160962006.dkr.ecr.your-region.amazonaws.com/scde:latest
+
+docker push 677160962006.dkr.ecr.us-east-1.amazonaws.com/scde:latest
