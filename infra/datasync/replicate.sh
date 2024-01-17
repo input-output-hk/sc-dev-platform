f351420eb835c33927bd7cae637b0f7c72fa243d
renebarbosafl <70078638+renebarbosafl@users.noreply.github.com>
2024-01-17 11:08:27 -0300

    Update replicate.sh

--- infra/datasync/replicate.sh
+++ infra/datasync/replicate.sh
@@ -6,30 +6,30 @@ SCHEMAS=(marlowe chain)
 [ -d /dump/chain ] && rm -rf /dump/chain
 
 fn_dump() {
-	ACTION=$1
-    DB_PASSWORD=$2
-    DB_HOST=$3
-    DB_USER=$4
-    DB_NAME=$5
-    DB_SCHEMA=$6
+  ACTION=$1
+  DB_PASSWORD=$2
+  DB_HOST=$3
+  DB_USER=$4
+  DB_NAME=$5
+  DB_SCHEMA=$6
 
-	export PGPASSWORD=$DB_PASSWORD
-	if [ $ACTION == "export" ]; then
-	    /usr/bin/pg_dump --host=$DB_HOST --username=$DB_USER --dbname=$DB_NAME --schema=$DB_SCHEMA \
-		--clean --format=d --file=/dump/$DB_SCHEMA --jobs=$CONCURRENCY_LEVEL --compress=$COMPRESS_LEVEL --verbose
-	else
-		/usr/bin/pg_restore --host=$DB_HOST --username=$DB_USER --dbname=$DB_NAME \
-		--format=d --jobs=$CONCURRENCY_LEVEL --clean /dump/$DB_SCHEMA --verbose
-	fi
+  export PGPASSWORD=$DB_PASSWORD
+  if [ $ACTION == "export" ]; then
+    /usr/bin/pg_dump --host=$DB_HOST --username=$DB_USER --dbname=$DB_NAME --schema=$DB_SCHEMA \
+    --clean --format=d --file=/dump/$DB_SCHEMA --jobs=$CONCURRENCY_LEVEL --compress=$COMPRESS_LEVEL --verbose
+  else
+    /usr/bin/pg_restore --host=$DB_HOST --username=$DB_USER --dbname=$DB_NAME \
+    --format=d --jobs=$CONCURRENCY_LEVEL --clean /dump/$DB_SCHEMA --verbose
+  fi
 }
 
 for SCHEMA in ${SCHEMAS[@]}; do
-	if [ $SCHEMA == "marlowe" ]; then
-		fn_dump "export" $MARLOWE_DB_PASSWORD $MARLOWE_DB_HOST $MARLOWE_DB_USER $MARLOWE_DB_NAME $SCHEMA
-	else
-		fn_dump "export" $CHAIN_DB_PASSWORD $CHAIN_DB_HOST $CHAIN_DB_USER $CHAIN_DB_NAME $SCHEMA
-	fi
-	fn_dump "import" $LOOKER_DB_PASSWORD $LOOKER_DB_HOST $LOOKER_DB_USER $LOOKER_DB_NAME $SCHEMA
+  if [ $SCHEMA == "marlowe" ]; then
+    fn_dump "export" $MARLOWE_DB_PASSWORD $MARLOWE_DB_HOST $MARLOWE_DB_USER $MARLOWE_DB_NAME $SCHEMA
+  else
+    fn_dump "export" $CHAIN_DB_PASSWORD $CHAIN_DB_HOST $CHAIN_DB_USER $CHAIN_DB_NAME $SCHEMA
+  fi
+  fn_dump "import" $LOOKER_DB_PASSWORD $LOOKER_DB_HOST $LOOKER_DB_USER $LOOKER_DB_NAME $SCHEMA
 done
 
 exit 0
