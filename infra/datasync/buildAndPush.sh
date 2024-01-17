e83a06b1dc3dccf537325922791e755cc53fda08
Renê Barbosa <renebarbosafl@gmail.com>
2024-01-17 11:04:46 -0300

    chore: adding adhoc resources for datasync work

---
+++ infra/datasync/buildAndPush.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Step 1: Build Docker Image
+DOCKER_IMAGE="data-sync:$(date +%Y%m%d)"
+docker build -t $DOCKER_IMAGE . --platform=linux/amd64
+
+# Step 2: AWS ECR Login
+export AWS_PROFILE=dapps-world
+account_id=$(aws sts get-caller-identity --output text --query 'Account')
+region=$(aws configure get region)
+ecr_registry="${account_id}.dkr.ecr.${region}.amazonaws.com"
+
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${region}.amazonaws.com
+
+# Step 3: Tag Docker Image
+docker tag $DOCKER_IMAGE ${ecr_registry}/$DOCKER_IMAGE
+
+# Step 4: Push Docker Image
+docker push ${ecr_registry}/$DOCKER_IMAGE
+
+# Additional Tips:
+echo "Make sure Docker has experimental features enabled for multi-platform builds."
+echo "Ensure AWS CLI is configured with the necessary permissions for Amazon ECR actions."
